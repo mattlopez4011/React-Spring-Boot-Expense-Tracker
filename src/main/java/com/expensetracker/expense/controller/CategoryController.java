64439c75927816a57331f3443eb016cd7d779462
Matt Lopez <matthew.lopez4011@gmail.com>
2020-02-17 21:28:24 -0600

    Create category controller. Start rest api

--- src/main/java/com/expensetracker/expense/controller/CategoryController.java
+++ src/main/java/com/expensetracker/expense/controller/CategoryController.java
@@ -1,4 +1,40 @@
 package com.expensetracker.expense.controller;
 
+import com.expensetracker.expense.model.Category;
+import com.expensetracker.expense.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository){
+        super();
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/categories")
+    Collection<Category> categories(){
+        return categoryRepository.findAll();
+        // select * from category
+    }
+
+    @GetMapping("/category/{id}")
+    ResponseEntity<?> getCategory(@PathVariable Long id){
+        Optional<Category> category = categoryRepository.findById(id);
+
+        // If a category is found it will send response, else send  404 not found .
+        return category.map( response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 }
--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -10,6 +10,3 @@ insert into expense values (100, 'New York', '2019-06-16T17:00:00.000Z',1,1);
 insert into expense values (101, 'Ford Mustang', '2019-06-15T15:00:00.000Z', 2,2);
 insert into expense values(102, 'Grand Canyon', '2019-06-14T15:00:00.000Z', 3,1);
 
-# Connect to H2 console by going to:
-# localhost:8080/h2-console
-# JDBC URL: jdbc:h2:mem:testdb
