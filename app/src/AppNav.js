db5c7a44a4f1b79af7f88d8bcfffdfce5218e2f2
Matt Lopez <matthew.lopez4011@gmail.com>
2020-02-25 09:06:41 -0600

    Add bootstrap, react router, react cookies dependencies

---
+++ app/src/AppNav.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText} from 'reactstrap';
+
+
+class AppNav  extends Component {
+    state = { }
+    render(){
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Expense Tracker Application</NavbarBrand>
+                    {/*<NavbarToggler onClick={toggle} />*/}
+                    {/*<Collapse isOpen={isOpen} navbar>*/}
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/home/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/categories">Categories</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/expenses">Expenses</NavLink>
+                            </NavItem>
+                            {/*<UncontrolledDropdown nav inNavbar>*/}
+                            {/*    <DropdownToggle nav caret>*/}
+                            {/*        Options*/}
+                            {/*    </DropdownToggle>*/}
+                            {/*    <DropdownMenu right>*/}
+                            {/*        <DropdownItem>*/}
+                            {/*            Option 1*/}
+                            {/*        </DropdownItem>*/}
+                            {/*        <DropdownItem>*/}
+                            {/*            Option 2*/}
+                            {/*        </DropdownItem>*/}
+                            {/*        <DropdownItem divider />*/}
+                            {/*        <DropdownItem>*/}
+                            {/*            Reset*/}
+                            {/*        </DropdownItem>*/}
+                            {/*    </DropdownMenu>*/}
+                            {/*</UncontrolledDropdown>*/}
+                        </Nav>
+                        <NavbarText>Simple Text</NavbarText>
+                    {/*</Collapse>*/}
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNav;
